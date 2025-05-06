f3b52b087c6157ed800d64cdd10a8109b2171464
Javier Sancho <sanchopej@gmail.com>
2025-05-06 17:31:53 +0200

    Imagen de fondo añadida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import type { Routes } from "@angular/router";
+import { HeroComponent } from "./components/hero/hero.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "", redirectTo: "/inicio", pathMatch: "full" },
+	{ path: "inicio", component: HeroComponent },
+	{ path: "**", redirectTo: "/inicio" },
+];
