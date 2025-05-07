5bab52ea2114b79afc9f01fd5574467b1cd7a2b9
Javier Sancho <sanchopej@gmail.com>
2025-05-07 17:23:01 +0200

    Ya funciona tambien las skills, tenia error en la ruta(projects en vez de skills)

--- src/app/components/sobre-mi/sobre-mi.component.ts
+++ src/app/components/sobre-mi/sobre-mi.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { SkillsService } from "../../services/skills.service";
+import type { Skill } from "../../interfaces/skills.interface";
 
 @Component({
-  selector: 'app-sobre-mi',
-  imports: [],
-  templateUrl: './sobre-mi.component.html',
-  styleUrl: './sobre-mi.component.css'
+	selector: "app-sobre-mi",
+	imports: [],
+	templateUrl: "./sobre-mi.component.html",
+	styleUrl: "./sobre-mi.component.css",
 })
 export class SobreMiComponent {
+	skillsService = inject(SkillsService);
+	skills: Skill[] = [];
 
+	async ngOnInit() {
+		this.skills = await this.skillsService.obtenerSkills();
+		console.log(this.skills);
+	}
 }
--- src/app/services/skills.service.ts
+++ src/app/services/skills.service.ts
@@ -1,6 +1,16 @@
-import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import type { Skill } from "../interfaces/skills.interface";
+import { lastValueFrom } from "rxjs";
 
 @Injectable({
 	providedIn: "root",
 })
-export class SkillsService {}
+export class SkillsService {
+	private apiUrl = "http://localhost:3000/api/skills";
+	private httpClient = inject(HttpClient);
+
+	obtenerSkills(): Promise<Skill[]> {
+		return lastValueFrom(this.httpClient.get<Skill[]>(this.apiUrl));
+	}
+}
