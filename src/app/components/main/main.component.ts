66401c8bf405c5c0b94484c73eb3dc79efb46d4e
Javier Sancho <sanchopej@gmail.com>
2025-05-08 19:07:51 +0200

    Portfolio terminado

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { SobreMiComponent } from "../sobre-mi/sobre-mi.component";
 import { ProyectosComponent } from "../proyectos/proyectos.component";
+import { ContactoComponent } from "../contacto/contacto.component";
 
 @Component({
 	selector: "app-hero",
 	standalone: true,
-	imports: [SobreMiComponent, ProyectosComponent],
+	imports: [SobreMiComponent, ProyectosComponent, ContactoComponent],
 	templateUrl: "./main.component.html",
 	styleUrls: ["./main.component.css"],
 })
